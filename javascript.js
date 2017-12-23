dd7f8e94da26f5a5b6c3143eb93a835bfa631a86
Darren Kwong <kwongdarren@gmail.com>
2017-12-22 23:46:00 -0800

    check user click with gameArray working

--- javascript.js
+++ javascript.js
@@ -6,9 +6,11 @@
 // function to create tiles
 // need to create additional tiles each level
 // function that checks if tile clicked is correct
-//  
+// 
+//  use set timeouts for delay between button blinks and animations 
 
-var gameArray = [0, 1, 0];
+var gameArray = [0, 1, 2, 3, 4];
+var userArray =[];
 
 var rand = Math.random();
 console.log(rand);
@@ -28,7 +30,6 @@ function generator () {
       var data = e.target.getAttribute('data')
       data = parseInt(data);
       console.log(typeof(data));
-
       //call check click function
       checkClick(data);
     } 
@@ -39,14 +40,42 @@ function generator () {
 
 //check if clicked button is correct
 function checkClick (data) {
-  if(data === 1) {
-    console.log('correct');
-    gameArray.push(1);
-    document.querySelector('.main').innerHTML= '';
-    generator();
-  } else {
-    console.log('wrong');
+
+  userArray.push(data);
+
+  for (var i = 0; i < userArray.length; i++) {
+    if(userArray[i] === gameArray[i]) {
+      console.log('correct');
+      // gameArray.push(1);
+      // gameArray = shuffle(gameArray); //randomize array indexes
+      // document.querySelector('.main').innerHTML= '';
+      // generator();
+    } else {
+      console.log('wrong');
+    }
   }
 }
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+arr = shuffle(gameArray);
+console.log(arr);
 
 generator();
